bd4f671cef95af34e24daa9cfb6b8c0ab405ce93
Sandeep Rao Ganga <gsr.sandeep94@gmail.com>
2026-07-22 16:21:58 +0530

    1st Commit

---
+++ Pages/Login.js
@@ -0,0 +1,21 @@
+exports.LoginPage = class LoginPage {
+
+    constructor(page) {
+        this.page = page
+        this.username_textbox = page.getByRole('textbox', { name: 'Username' })
+        this.password_textbox = page.getByRole('textbox', { name: 'Password' })
+        this.Login_button = page.getByRole('button', { name: ' Login' })
+    }
+
+    async gotoLoginPage() {
+
+        await this.page.goto('https://the-internet.herokuapp.com/login');
+    }
+
+    async login(username,password) {
+        await this.username_textbox.fill(username)
+        await this.password_textbox.fill(password)
+        await this.Login_button.click()
+    }
+
+}
